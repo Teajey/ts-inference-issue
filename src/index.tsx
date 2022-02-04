242bdbb72d17463b70a78ef3616007cb58c4cf73
Teajey <21069848+Teajey@users.noreply.github.com>
2022-02-05 11:29:09 +1300

    Initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+function useThing<T = any, U = any>({
+  something,
+  callback,
+}: {
+  something: T;
+  callback: (a: T) => U;
+}) {
+  return callback(something);
+}
+
+function ThingComponent<T = any, U = any>({
+  something,
+  callback,
+  children,
+}: {
+  something: T;
+  callback: (a: T) => U;
+  children: (r: U) => JSX.Element;
+}) {
+  const result = useThing({ something, callback });
+  return <div>{children(result)}</div>;
+}
+
+function App() {
+  useThing({ something: 1, callback: (s) => s + "1" });
+
+  return (
+    <ThingComponent something={1} callback={(s) => s + "1"}>
+      {(r) => <div>{r}</div>}
+    </ThingComponent>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
